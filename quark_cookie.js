793dd5b0bc9fb74a8547d0a88047cfee1b2e6dcb
MrAcT-Cat <55550894+MrAcT-Cat@users.noreply.github.com>
2026-05-05 22:15:05 +0800

    Update quark_cookie.js

--- quark_cookie.js
+++ quark_cookie.js
@@ -1,29 +1,19 @@
-// 夸克抓包（最终版）
-const storeKey = "quark_sign_account_v2";
+const scriptName = "夸克抓包";
+const storeKey = "quark_scan_data";
 
-function notify(t, s) {
-  $notification.post("夸克抓包", t, s);
+function notify(title, body) {
+  $notification.post(scriptName, title, body);
 }
 
-if ($request) {
-
-  const url = $request.url;
-
-  // ✅ 只抓真正签到接口
-  if (url.indexOf("pOcKNmTupWelFare") !== -1) {
-
-    const data = {
-      url: url,
-      headers: $request.headers,
-      body: $request.body,   // ⭐关键
-      time: Date.now()
-    };
-
-    $persistentStore.write(JSON.stringify(data), storeKey);
-
-    notify("✅ 抓取成功", "已获取签到参数");
-  }
-
+if ($request && $request.method == "POST" && $request.url.includes("uc_param_str=")) {
+  let data = {
+    url: $request.url,
+    headers: $request.headers,
+    body: $request.body,
+    ts: Date.now()
+  };
+  $persistentStore.write(JSON.stringify(data), storeKey);
+  notify("✅ 抓取成功", "已保存有效签到参数");
 }
 
 $done({});
