be6898784d705ff886187875d835696c2bb23c2d
MrAcT-Cat <55550894+MrAcT-Cat@users.noreply.github.com>
2026-05-05 15:02:29 +0800

    更新 quark_cookie.js

--- quark_cookie.js
+++ quark_cookie.js
@@ -1,5 +1,10 @@
 let url = $request.url;
 let body = $request.body || "";
+let cookie = $request.headers.Cookie || $request.headers.cookie;
+
+if (cookie) {
+  $persistentStore.write(cookie, "quark_cookie");
+}
 
 $persistentStore.write(url, "quark_url");
 $persistentStore.write(body, "quark_body");
