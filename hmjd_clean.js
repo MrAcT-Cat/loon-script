cf2190c9e51b3a937f6cff1a9859c47089f1ee66
MrAcT-Cat <55550894+MrAcT-Cat@users.noreply.github.com>
2026-07-11 20:43:40 +0800

    更新 hmjd_clean.js

--- hmjd_clean.js
+++ hmjd_clean.js
@@ -1,13 +1,27 @@
-$done({
-    body: $response.body + `
-    <script>
-    window.addEventListener('load', ()=>{
-        setInterval(()=>{
-            document.querySelectorAll('.fixed, [style*="position:fixed"]').forEach(el=>{
-                el.remove();
-            })
-        }, 200);
-    })
-    </script>
-    `
-});
+// 对齐B站脚本风格：Loon / QuanX 双兼容 http-response 清理悬浮广告
+const isQuanX = typeof $task !== 'undefined';
+
+let body = $response.body;
+
+const injectScript = [
+    '<script>',
+    '(function(){',
+    'function clearFloatAds(){',
+    '    var els = document.querySelectorAll(".fixed, [style*=\\"position:fixed\\"]");',
+    '    for(var i = 0; i < els.length; i++){',
+    '        els[i].remove();',
+    '    }',
+    '}',
+    'clearFloatAds();',
+    'setInterval(clearFloatAds, 200);',
+    '})();',
+    '</script>'
+].join('');
+
+body = body + injectScript;
+
+if (isQuanX) {
+    $done({ body });
+} else {
+    $done({ body: body });
+}
